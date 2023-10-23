6121615c7ff178ba116ffea52df2e9e30a60f0df
Oleksandr B <aleber70@meta.ua>
2023-10-24 01:55:05 +0300

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,28 +12,26 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
-
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-pygments_style = None
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
-
-latex_elements = {
-}
-
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
-epub_title = project
-
-epub_exclude_files = ['search.html']
 
 html_favicon = "img/favicon.png"
