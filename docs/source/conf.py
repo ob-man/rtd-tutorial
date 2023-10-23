c12eb7e320e1164b465c3d0fc01b71d61c5da366
Oleksandr B <aleber70@meta.ua>
2023-10-24 00:57:18 +0300

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,9 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
@@ -33,3 +26,5 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_favicon = "img/favicon.png"
